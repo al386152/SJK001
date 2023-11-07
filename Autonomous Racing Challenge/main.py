39c08567893f1533fe56a3a880363308af5f24a9
Paul <al386152@uji.es>
2023-11-07 11:45:35 +0100

    Apaño con un try-except e intento de apaño para los giros

--- Autonomous Racing Challenge/main.py
+++ Autonomous Racing Challenge/main.py
@@ -14,19 +14,23 @@ def get_mask(min_range=(0, 125, 125), max_range=(30, 255, 255)):
     red_mask = cv2.inRange(hsv, min_range, max_range)
     return red_mask
 
+
 # TODO: falla aquí cuando no detecta la línea en, por ejemplo, las curvas
 #   IndexError: tuple index out of range.
 def get_momentums(red_mask):
-    contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    return cv2.moments(contours[0])
+    try:
+        contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        return cv2.moments(contours[0])
+    except:
+        return None
 
 
 def get_centroids(M):
-    if M["m00"] != 0:
+    cX, cY = 0, 0
+
+    if M and M["m00"] != 0:
         cX = M["m10"] / M["m00"]
         cY = M["m01"] / M["m00"]
-    else:
-        cX, cY = 0, 0
 
     return cX, cY
 
@@ -39,7 +43,6 @@ velocidad_recta = 6
 def get_velocidades(cX, cX_anterior, velocidad_anterior):
     # Si la línea está a la derecha vale más de 320
     # Si la línea está a la izquierda, vale menos de 320
-
     print(f"cX_anterior: {cX_anterior}, velocidad_anterior: {velocidad_anterior}")
     if cX == 0:
         # Estado: Línea perdida.
@@ -59,8 +62,8 @@ def get_velocidades(cX, cX_anterior, velocidad_anterior):
         # Estado: línea a los lados.
         # Si es una curva, lo mejor es frenar
         # TODO: Los siguientes valores deberían ser en función
-        #vel_lineal = velocidad_anterior * 0.1
-        vel_lineal = velocidad_recta #* (cX - cX_anterior)
+        # vel_lineal = velocidad_anterior * 0.1
+        vel_lineal = velocidad_recta  # * (cX - cX_anterior)
         vel_angular = 0.01 * (cX_anterior - cX)
         if cX > pos_centro:
             # Estado: línea a la derecha
@@ -69,8 +72,12 @@ def get_velocidades(cX, cX_anterior, velocidad_anterior):
             # Estado: línea a la izquierda
             print("Línea a la izquierda")
 
+    # Esto es para forzar el giro
+    if (cX_anterior - cX) != 0:
+        cX_anterior = cX
+
     print(f"vel_lineal: {vel_lineal}, vel_angular: {vel_angular}")
-    return (vel_lineal, vel_angular)
+    return (vel_lineal, vel_angular, cX_anterior)
 
 
 i = 0
@@ -80,8 +87,7 @@ while True:
     red_mask = get_mask()
     cX, cY = get_centroids(get_momentums(red_mask))
 
-    vel, rotacion = get_velocidades(cX, cX_anterior, vel)
-    cX_anterior = cX
+    vel, rotacion, cX_anterior = get_velocidades(cX, cX_anterior, vel)
 
     HAL.setV(vel)
     HAL.setW(rotacion)
