bea4fcba9357b42d5fa4b7df2226eeb1e0356baa
Paul <al386152@uji.es>
2023-11-07 17:41:45 +0100

    Más o menos sigue la línea, pero es necesario mirar como puedo ajustar bien las constantes

--- Autonomous Racing Challenge/main.py
+++ Autonomous Racing Challenge/main.py
@@ -2,9 +2,13 @@ from GUI import GUI
 from HAL import HAL
 # Enter sequential code!
 
-
 import cv2
 import time
+
+# Constantes:
+pos_centro = 320
+velocidad_recta = 6
+ajuste_error_inicial = 0.005
 
 
 def get_mask(min_range=(0, 125, 125), max_range=(30, 255, 255)):
@@ -15,8 +19,6 @@ def get_mask(min_range=(0, 125, 125), max_range=(30, 255, 255)):
     return red_mask
 
 
-# TODO: falla aquí cuando no detecta la línea en, por ejemplo, las curvas
-#   IndexError: tuple index out of range.
 def get_momentums(red_mask):
     try:
         contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -35,23 +37,19 @@ def get_centroids(M):
     return cX, cY
 
 
-# Estado de la lína (si está perdida, delante, etc.)
-pos_centro = 320
-velocidad_recta = 6
-
-
-def get_velocidades(cX, cX_anterior, velocidad_anterior):
+def get_velocidades(cX, prev_error, ajuste_error):
     # Si la línea está a la derecha vale más de 320
     # Si la línea está a la izquierda, vale menos de 320
-    print(f"cX_anterior: {cX_anterior}, velocidad_anterior: {velocidad_anterior}")
+    error = pos_centro - cX
+    #ajuste_error = (prev_error - error) * ajuste_error
+    ajuste_error = ajuste_error_inicial
     if cX == 0:
         # Estado: Línea perdida.
         print("Línea a la perdida")
 
         # Si se pierde la línea, gira hasta encontrarla otra vez.
-        vel_lineal = 0
-        # Si hemos perido la línea, gira en la dirección opuesta a la última posición conocida.
-        vel_angular = -cX_anterior
+        vel_lineal = 0.5
+        vel_angular = 1
 
     elif cX == pos_centro:
         # Estado: línea delante.
@@ -61,10 +59,8 @@ def get_velocidades(cX, cX_anterior, velocidad_anterior):
     else:
         # Estado: línea a los lados.
         # Si es una curva, lo mejor es frenar
-        # TODO: Los siguientes valores deberían ser en función
-        # vel_lineal = velocidad_anterior * 0.1
-        vel_lineal = velocidad_recta  # * (cX - cX_anterior)
-        vel_angular = 0.01 * (cX_anterior - cX)
+        vel_lineal = velocidad_recta * (1 - (error / pos_centro))
+        vel_angular = error * ajuste_error
         if cX > pos_centro:
             # Estado: línea a la derecha
             print("Línea a la derecha")
@@ -72,26 +68,29 @@ def get_velocidades(cX, cX_anterior, velocidad_anterior):
             # Estado: línea a la izquierda
             print("Línea a la izquierda")
 
-    # Esto es para forzar el giro
-    if (cX_anterior - cX) != 0:
-        cX_anterior = cX
-
     print(f"vel_lineal: {vel_lineal}, vel_angular: {vel_angular}")
-    return (vel_lineal, vel_angular, cX_anterior)
+    return (vel_lineal, vel_angular, error, ajuste_error)
 
 
 i = 0
-cX_anterior = pos_centro
+rotacion = 0
+error = 0
 vel = velocidad_recta
+ajuste_error = ajuste_error_inicial
 while True:
     red_mask = get_mask()
     cX, cY = get_centroids(get_momentums(red_mask))
 
-    vel, rotacion, cX_anterior = get_velocidades(cX, cX_anterior, vel)
+    vel, rotacion, error, ajuste_error = get_velocidades(cX, error, ajuste_error)
 
     HAL.setV(vel)
     HAL.setW(rotacion)
+    """HAL.setV(0)
+    HAL.setW(0)
+
+    time.sleep(2)"""
 
     GUI.showImage(red_mask)
     print('%d, cX: %.2f cY: %.2f' % (i, cX, cY))
     i += 1
+
