4a1a05a78ce8b660c7ae7951787807bbc59fbc22
Paul <al386152@uji.es>
2023-11-07 11:11:39 +0100

    No se mueve, pero la idea está ahí (es decir, falta afinar los parámetros)

--- Autonomous Racing Challenge/main.py
+++ Autonomous Racing Challenge/main.py
@@ -2,63 +2,90 @@ from GUI import GUI
 from HAL import HAL
 # Enter sequential code!
 
-   
+
 import cv2
 import time
 
-def get_momentums():
-  img = HAL.getImage()
-   
+
+def get_mask(min_range=(0, 125, 125), max_range=(30, 255, 255)):
+    img = HAL.getImage()
+
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    red_mask = cv2.inRange(hsv,
-                            (0, 125, 125),    
-                            (30, 255, 255))
-   
-    contours, hierarchy = cv2.findContours(red_mask,
-                                            cv2.RETR_TREE,
-                                            cv2.CHAIN_APPROX_SIMPLE)
-   
+    red_mask = cv2.inRange(hsv, min_range, max_range)
+    return red_mask
+
+
+def get_momentums(red_mask):
+    contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return cv2.moments(contours[0])
- 
-  def get_centroids(M):
+
+
+def get_centroids(M):
     if M["m00"] != 0:
-      cX = M["m10"] / M["m00"]
-      cY = M["m01"] / M["m00"]
+        cX = M["m10"] / M["m00"]
+        cY = M["m01"] / M["m00"]
     else:
-      cX, cY = 0, 0
-     
+        cX, cY = 0, 0
+
     return cX, cY
 
 
 # Estado de la lína (si está perdida, delante, etc.)
-ESTADO_PERDIDA    = -1
-ESTADO_DELANTE    = 0
-ESTADO_DERECHA    = 1
-ESTADO_IZQUIERDA  = 2
+pos_centro = 320
+velocidad_recta = 1
+
+
+def get_velocidades(cX, cX_anterior, velocidad_anterior):
+    # Si la línea está a la derecha vale más de 320
+    # Si la línea está a la izquierda, vale menos de 320
+
+    print(f"cX_anterior: {cX_anterior}, velocidad_anterior: {velocidad_anterior}")
+    if cX == 0:
+        # Estado: Línea perdida.
+        print("Línea a la perdida")
+
+        # Si se pierde la línea, gira hasta encontrarla otra vez.
+        vel_lineal = 0
+        # Si hemos perido la línea, gira en la dirección opuesta a la última posición conocida.
+        vel_angular = -cX_anterior
+
+    elif cX == pos_centro:
+        # Estado: línea delante.
+        vel_lineal = velocidad_recta
+        vel_angular = 0
+        print("Línea delante")
+    else:
+        # Estado: línea a los lados.
+        # Si es una curva, lo mejor es frenar
+        # TODO: Los siguientes valores deberían ser en función
+        vel_lineal = velocidad_anterior * 0.1
+        vel_angular = 0.01 * (pos_centro - cX)
+        if cX > pos_centro:
+            # Estado: línea a la derecha
+            print("Línea a la derecha")
+        else:  # if cX < pos_centro:
+            # Estado: línea a la izquierda
+            print("Línea a la izquierda")
+
+    print(f"vel_lineal: {vel_lineal}, vel_angular: {vel_angular}")
+    return (vel_lineal, vel_angular)
 
 
 i = 0
-speed = 1
-val = 320
-
-estado = ESTADO_PERDIDA
-
+cX_anterior = pos_centro
+vel = velocidad_recta
 while True:
-   
-    cX, cY = get_centroids(get_momentums())
-   
-    # Si la línea está a la derecha vale más de 320
-      # Si la línea está a la izquierda, vale menos de 320  
-     
-    if cX > 0:
-      err = val - cX
-     
-     
-      #HAL.setV(speed)
-      #HAL.setW(0.01 * err)
-     
+    red_mask = get_mask()
+    cX, cY = get_centroids(get_momentums(red_mask))
+
+    vel, rotacion = get_velocidades(cX, cX_anterior, vel)
+    cX_anterior = cX
+
+    HAL.setV(vel)
+    HAL.setW(rotacion)
+
     GUI.showImage(red_mask)
-    print('%d, cX: %.2f cY: %.2f' % (i, cX, cY) )
+    print('%d, cX: %.2f cY: %.2f' % (i, cX, cY))
     i += 1
 
 
