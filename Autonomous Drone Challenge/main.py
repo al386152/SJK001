7025b6ca19a1ec5926963c2d9bd9292511f34184
Paul <al386152@uji.es>
2023-11-28 19:16:22 +0100

    añadidos filtros de color verde y azúl

--- Autonomous Drone Challenge/main.py
+++ Autonomous Drone Challenge/main.py
@@ -17,12 +17,19 @@ def get_ventral_image():
     return HAL.get_ventral_image()
 
 
-def get_mask(min_range=(0, 125, 125), max_range=(30, 255, 255)):
-    img = HAL.getImage()
-
+def get_mask(img=HAL.get_frontal_image(), min_range=(0, 125, 125), max_range=(30, 255, 255)):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    red_mask = cv2.inRange(hsv, min_range, max_range)
-    return red_mask
+    mask = cv2.inRange(hsv, min_range, max_range)
+    return mask
+
+
+# https://i.stack.imgur.com/TSKh8.png
+def get_green_mask(img):
+    return get_mask(img, min_range=(36, 25, 25), max_range=(70, 255, 255))
+
+
+def get_blue_mask(img):
+    return get_mask(img, min_range=(85, 25, 140), max_range=(150, 255, 255))
 
 
 def get_momentums(red_mask):
@@ -52,26 +59,29 @@ def parar(t):
 
 
 def print_state():
-    print(f"position: {HAL.get_position()}")
-    print(f"velocity: {HAL.get_velocity()}")
-    print(f"yaw_rate: {HAL.get_yaw_rate()}")
-    print(f"orientation: {HAL.get_orientation()}")
-    print(f"roll: {HAL.get_roll()}")
-    print(f"pitch: {HAL.get_pitch()}")
-    print(f"yaw: {HAL.get_yaw()}")
-    print(f"landed_state: {HAL.get_landed_state()}")
+    print(f"get_position: {HAL.get_position()}")
+    print(f"get_velocity: {HAL.get_velocity()}")
+    print(f"get_yaw_rate: {HAL.get_yaw_rate()}")
+    print(f"get_orientation: {HAL.get_orientation()}")
+    print(f"get_roll: {HAL.get_roll()}")
+    print(f"get_pitch: {HAL.get_pitch()}")
+    print(f"get_yaw: {HAL.get_yaw()}")
+    print(f"get_landed_state: {HAL.get_landed_state()}")
     print("-----")
 
 
+i = 0
 while True:
     frontal = get_frontal_image()
     ventral = get_ventral_image()
 
     print_state()
-    mover_posicion_respecto_barco(-2, 2, 10, 0)
+    # HAL.takeoff(1)
+    mover_posicion_respecto_barco(0, 0, 10, 0)
+    # mover_posicion_respecto_barco(1, 2, 5, 0)
 
-    GUI.showImage(frontal)
-    GUI.showLeftImage(ventral)
-    i += 1
+    # GUI.showImage(get_green_mask(frontal))
+    GUI.showLeftImage(get_blue_mask(ventral))
+    GUI.showImage(get_blue_mask(ventral))
+    # GUI.showLeftImage(ventral)
 
-    # if i % 20 == 0: parar(1)
