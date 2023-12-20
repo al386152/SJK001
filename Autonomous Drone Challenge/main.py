27f4fbc0c98d811180da89406c7c6e77fc38d88a
Paul <al386152@uji.es>
2023-12-20 22:03:47 +0100

    Tenemos los Bounding Boxes. Ahora estoy tratando de ponerme encima de los naufragos

--- Autonomous Drone Challenge/main.py
+++ Autonomous Drone Challenge/main.py
@@ -10,6 +10,7 @@ NUM_NAUFRAGOS = 6
 ALTURA_VUELO = 25
 X_RESCATE = 35
 Y_RESCATE = -35
+FORMA_IMAGEN = (240, 320) #, 3) # Obtenida de forma euristica
 
 def get_frontal_image():
     return HAL.get_frontal_image()
@@ -17,17 +18,14 @@ def get_frontal_image():
 def get_ventral_image():
     return HAL.get_ventral_image()
 
-
 def get_mask(img=HAL.get_frontal_image(), min_range=(0, 125, 125), max_range=(30, 255, 255)):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, min_range, max_range)
     return mask
 
-
 # https://i.stack.imgur.com/TSKh8.png
 def get_green_mask(img):
     return get_mask(img, min_range=(36, 25, 25), max_range=(70, 255, 255))
-
 
 def get_blue_mask(img):
     return get_mask(img, min_range=(85, 25, 140), max_range=(150, 255, 255))
@@ -49,18 +47,22 @@ def moverse_a_la_zona_del_rescate():
     # Posición obtenida de forma heurística
     mover_posicion_respecto_barco(X_RESCATE, Y_RESCATE, ALTURA_VUELO, 0)
 
+def moverse_en_cierta_direccion(vx=None, vy=None, vz=None, vyaw=None):
+    # Prefiero poner los valores en funciones de [-1,1]
+
+    x = vx * (vx is not None)
+    y = vy * (vy is not None)
+    z = vz * (vz is not None)
+    yaw = vyaw * (vyaw is not None)
+
+    HAL.set_cmd_vel(x, y, z, yaw)
+
 def check(pos_a, pos_b, error = 0.5):
     return abs(pos_a - pos_b) < error
 
 def is_in_position(x, y, z, error=0.5):
     pos_x, pos_y, pos_z = HAL.get_position()
     return (x is None or check(pos_x, x, error)) and (y is None or check(pos_y, y, error)) and (z is None or check(pos_z, z, error))
-
-def establecer_velocidad(x = 0, y = 0, z = 0):
-    HAL.set_cmd_vel(x, y, z, 0)
-
-def hay_naufrago():
-    return False
 
 def reconocer_cara(src):
     # Si se reconoce la car
@@ -86,47 +88,28 @@ def reconocer_cara(src):
         #     cv2.rectangle(roi_src, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
     return faces
 
-# Esto es para tener un esquema.
-def avanzar_hasta_posicion_y_comprobacion(x, y, paso = 1):
-    while not is_in_position(x, y, None):
-        establecer_velocidad(x=paso * (not x is None), y=paso * (not y is None))
-        imagen = mostrar_imagen_ventral_como_yo_quiero()
-        if hay_naufrago():
-            parar()
-            caras = reconocer_cara(imagen)
-            if len(caras) != 0:
-                return caras_naufragos.add(caras[0])
-
-def vueltas_en_cuadrados(avanze = 1, pseudo_radio = 1, pos_actual = HAL.get_position()):
-    x, y, z = pos_actual
-    # La idea es, avanzamos un poco, damos una vuelta, avanzamos otro poco, damos otra vuelta
-    mover_posicion_respecto_barco(x, y + avanze, z, 0)
-    mover_posicion_respecto_barco(x, y + pseudo_radio, z, 0)
-
 def esperar(t):
     time.sleep(t)
-
-
 def parar():
     HAL.set_cmd_vel(0, 0, 0, 0)
-
-
 def despega():
-    HAL.takeoff(10)
-
+    HAL.takeoff(ALTURA_VUELO)
 
 def print_state():
     print(f"get_position: {HAL.get_position()}")
-    # Parece que no va bien la siguiente función, al menos al iniciar:
-    # print(f"get_velocity: {HAL.get_velocity()}")
-    # Parece que no va bien la siguiente función, al menos al iniciar:
-    # print(f"get_yaw_rate: {HAL.get_yaw_rate()}")
     print(f"get_orientation: {HAL.get_orientation()}")
     print(f"get_roll: {HAL.get_roll()}")
     print(f"get_pitch: {HAL.get_pitch()}")
     print(f"get_yaw: {HAL.get_yaw()}")
     print(f"get_landed_state: {HAL.get_landed_state()}")
     print("-----")
+
+def get_posiciones_naufragos(mask):
+    posiciones = list()
+    for tup_centro_x_y, tup_width_height, angle in get_bounding_boxes(mask):
+        x, y = tup_centro_x_y
+        posiciones.append((x, y, angle))
+    return posiciones
 
 def mostrar_imagen_ventral_como_yo_quiero():
     ventral = get_ventral_image()
@@ -136,23 +119,47 @@ def mostrar_imagen_ventral_como_yo_quiero():
     return ventral, mask
 
 
+def datos_normalizados(l):
+    minim = min(l)
+    maxim = max(l)
+    dif = maxim - minim
+
+    return [(2 * ((v - minim) / dif) - 1) for v in l]
+
+def ir_al_naufrago(pos):
+    pass
+
 print_state()
 despega()
-
+# Primero vamos al sitio
 while not is_in_position(X_RESCATE, Y_RESCATE, ALTURA_VUELO):
-    # frontal = get_frontal_image()
     mostrar_imagen_ventral_como_yo_quiero()
-
     moverse_a_la_zona_del_rescate()
     print_state()
 
-while True:
-    img, mask = mostrar_imagen_ventral_como_yo_quiero()
-    print(get_bounding_boxes(mask))
-    esperar(5)
+pos_momento_captura = HAL.get_position()
+img, mask = mostrar_imagen_ventral_como_yo_quiero()
+print(get_posiciones_naufragos(mask))
+x, y, z, yaw = pos_momento_captura
 
-caras_naufragos = set()
-num_rescatados = 0
+while not is_in_position(x + 1, y, z, error=0):
+    mover_posicion_respecto_barco(x + 1, y, z, 0)
+    print_state()
+img, mask = mostrar_imagen_ventral_como_yo_quiero()
+print(get_posiciones_naufragos(mask))
+
+
+
+# Luego, no paramos de buscar los naufragos hasta que no los hayamos encontrado todos
+
+#dict_naufragos = dict()
+#num_rescatados = 0
 #while num_rescatados < NUM_NAUFRAGOS:
+#    img, mask = mostrar_imagen_ventral_como_yo_quiero()
+#    pos = get_posiciones_naufragos(mask)
+    # Como no sé bien cómo transormar de píxeles a metros teniendo en cuenta la perspectiva
+    #  El plan es que se mueva en una dirección hasta que esté justo encima.
+
+
 
 
