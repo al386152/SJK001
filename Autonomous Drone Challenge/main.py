32c413193dd1fc4524a3e37d9a5feddb687d1131
Paul <al386152@uji.es>
2023-12-20 14:33:39 +0100

    Comenzando a hacer una posible implementación

--- Autonomous Drone Challenge/main.py
+++ Autonomous Drone Challenge/main.py
@@ -6,12 +6,13 @@ import cv2
 import time
 
 # Constantes:
-pos_barco = [0, 0, 0]
-
+NUM_NAUFRAGOS = 6
+ALTURA_VUELO = 10
+X_RESCATE = 35
+Y_RESCATE = -35
 
 def get_frontal_image():
     return HAL.get_frontal_image()
-
 
 def get_ventral_image():
     return HAL.get_ventral_image()
@@ -31,6 +32,8 @@ def get_green_mask(img):
 def get_blue_mask(img):
     return get_mask(img, min_range=(85, 25, 140), max_range=(150, 255, 255))
 
+def get_not_blue_mask(img):
+    return ~get_blue_mask(img)
 
 def get_momentums(red_mask):
     try:
@@ -49,12 +52,76 @@ def get_centroids(M):
 
     return cX, cY
 
-
 def mover_posicion_respecto_barco(x, y, z, yaw):
     HAL.set_cmd_pos(x, y, z, yaw)
 
 def moverse_a_la_zona_del_rescate():
-    mover_posicion_respecto_barco(35, -35, 10, 0)
+    # Posición obtenida de forma heurística
+    mover_posicion_respecto_barco(X_RESCATE, -Y_RESCATE, ALTURA_VUELO, 0)
+
+def check(pos_a, pos_b, error = 0.5):
+    return abs(pos_a - pos_b) < error
+
+def is_in_position(x, y, z, error=0.5):
+    pos_x, pos_y, pos_z = HAL.get_position()
+    return (x is None or check(pos_x, x, error)) and (y is None or check(pos_y, y, error)) and (z is None or check(pos_z, z, error))
+
+def establecer_velocidad(x = 0, y = 0, z = 0):
+    HAL.set_cmd_vel(x, y, z, 0)
+
+def hay_naufrago():
+    return False
+
+def reconocer_cara(src):
+    # Si se reconoce la car
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+    # Load pre-trained classifiers
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    #eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+    # Detect faces
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(0, 0))
+
+    # for (x, y, w, h) in faces:
+        #roi_gray = gray[y:y + h, x:x + w]
+        #roi_src = src[y:y + h, x:x + w]
+
+        # Draw rectangle around face
+        # cv2.rectangle(src, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # # Detect eyes in face ROI
+        # eyes = eyeCascade.detectMultiScale(roi_gray)
+        # for (ex, ey, ew, eh) in eyes:
+        #     # Draw rectangle around eyes
+        #     cv2.rectangle(roi_src, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
+    return faces
+
+# Esto es para tener un esquema.
+def avanzar_hasta_posicion_y_comprobacion(x, y, paso = 1):
+    while not is_in_position(x, y, None):
+        establecer_velocidad(x=paso * (not x is None), y=paso * (not y is None))
+        imagen = mostrar_imagen_ventral_como_yo_quiero()
+        if hay_naufrago():
+            parar()
+            caras = reconocer_cara(imagen)
+            if len(caras) != 0:
+                return caras_naufragos.add(caras[0])
+
+def vueltas_en_cuadrados(avanze = 1, pseudo_radio = 1, pos_actual = HAL.get_position()):
+    x, y, z = pos_actual
+    # La idea es, avanzamos un poco, damos una vuelta, avanzamos otro poco, damos otra vuelta
+    mover_posicion_respecto_barco(x, y + avanze, z, 0)
+
+
+    mover_posicion_respecto_barco(x, y + pseudo_radio, z, 0)
+
+
+def escanar_cara():
+    cara = None
+    return cara
+
+def naufrago_encontrado():
+    return False
 
 def esperar(t):
     time.sleep(t)
@@ -70,8 +137,10 @@ def despega():
 
 def print_state():
     print(f"get_position: {HAL.get_position()}")
-    #print(f"get_velocity: {HAL.get_velocity()}") # Parece que no va bien, al menos al iniciar.
-    #print(f"get_yaw_rate: {HAL.get_yaw_rate()}") # Parece que no va bien, al menos al iniciar.
+    # Parece que no va bien la siguiente función, al menos al iniciar:
+    # print(f"get_velocity: {HAL.get_velocity()}")
+    # Parece que no va bien la siguiente función, al menos al iniciar:
+    # print(f"get_yaw_rate: {HAL.get_yaw_rate()}")
     print(f"get_orientation: {HAL.get_orientation()}")
     print(f"get_roll: {HAL.get_roll()}")
     print(f"get_pitch: {HAL.get_pitch()}")
@@ -79,26 +148,25 @@ def print_state():
     print(f"get_landed_state: {HAL.get_landed_state()}")
     print("-----")
 
-
-# print_state()
-despega()
-while True:
-    frontal = get_frontal_image()
+def mostrar_imagen_ventral_como_yo_quiero():
     ventral = get_ventral_image()
+    GUI.showImage(ventral)
+    GUI.showLeftImage(get_not_blue_mask(ventral))
+    return ventral
+
+
+print_state()
+despega()
+
+while not is_in_position(X_RESCATE, Y_RESCATE, ALTURA_VUELO):
+    # frontal = get_frontal_image()
+    mostrar_imagen_ventral_como_yo_quiero()
 
     moverse_a_la_zona_del_rescate()
     print_state()
-    # mover_posicion_respecto_barco(1, 2, 5, 0)
-    # esperar(2)
-    # print(f"get_position: {HAL.get_position()}")
-    # HAL.set_cmd_vel(10, 10, 10, 10)
-    # print(f"get_position: {HAL.get_position()}")
 
-    print(f"get_blue_mask: {get_blue_mask(ventral)}")
-    print(f"not get_blue_mask: {~get_blue_mask(ventral)}")
+caras_naufragos = set()
+num_rescatados = 0
+while num_rescatados < NUM_NAUFRAGOS:
 
-    # GUI.showImage(get_green_mask(frontal))
-    GUI.showImage(get_blue_mask(frontal))
-    GUI.showLeftImage(get_blue_mask(ventral))
-    # GUI.showLeftImage(ventral)
 
